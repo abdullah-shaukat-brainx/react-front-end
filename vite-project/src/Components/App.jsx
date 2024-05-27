f626adc2a88f6f0fc5b95255c0ff8c616fcfd156
abdullah <abdullah.shaukat@brainxtech.com>
2024-05-27 19:39:26 +0500

    login and signup pasges

---
+++ vite-project/src/Components/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import SignUpForm from "./Signup";
+import LogInForm from "./Login";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <LogInForm />
+    </>
+  );
+}
+
+export default App;
