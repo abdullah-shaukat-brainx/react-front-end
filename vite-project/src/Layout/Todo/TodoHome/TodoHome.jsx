95dfd94f77f1f3f91062f7e5d806996ea61ebd75
abdullah <abdullah.shaukat@brainxtech.com>
2024-06-05 14:38:25 +0500

    Filtering Results + Pagination Added

--- vite-project/src/Layout/Todo/TodoHome/TodoHome.jsx
+++ vite-project/src/Layout/Todo/TodoHome/TodoHome.jsx
@@ -6,21 +6,57 @@ import "./TodoHome.css";
 import TodoItem from "../TodoItem/TodoItem";
 import { InfinitySpin } from "react-loader-spinner";
 import { useNavigate } from "react-router";
+import { useSearchParams } from "react-router-dom";
+import Pagination from "@mui/material/Pagination";
 
 function TodoHome() {
   const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
   const [spinner, setSpinner] = useState(false);
   const [todos, setTodos] = useState([]);
-
+  const [searchQuery, setSearchQuery] = useState("");
   const [refresh, setRefresh] = useState();
+  const [totalPagesCount, setTotalPagesCount] = useState(1);
   const updateRefresh = () => {
     setRefresh(!refresh);
   };
 
-  const fetchTodos = async () => {
+  const useDebouncedValue = (inputValue, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(inputValue);
+  
+    useEffect(() => {
+      const handler = setTimeout(() => {
+        setDebouncedValue(inputValue);
+      }, delay);
+  
+      return () => {
+        clearTimeout(handler);
+      };
+    }, [inputValue, delay]);
+  
+    return debouncedValue;
+  };
+
+  const debouncedSearchQuery = useDebouncedValue(searchQuery, 500);
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const [currentPage, setCurrentPage] = useState(
+    parseInt(searchParams?.get("page")) || 1
+  );
+  const [limit, setLimit] = useState(parseInt(searchParams?.get("limit")) || 5);
+
+  const fetchTodos = async (page, limit) => {
     setSpinner(true);
     try {
-      const response = await getTodos();
+      const response = await getTodos({
+        searchQuery: searchQuery.trim(),
+        page: page,
+        limit: limit,
+      });
+      setTotalPagesCount(parseInt(response?.data?.count));
       setTodos(response?.data?.Todos);
     } catch (error) {
       toast.error(`${error?.response?.data?.error}` || "Unable to Fetch Todos");
@@ -28,14 +64,23 @@ function TodoHome() {
       setSpinner(false);
     }
   };
+
   useEffect(() => {
-    fetchTodos();
-  }, [refresh]);
+    fetchTodos(currentPage, limit);
+  }, [refresh, debouncedSearchQuery, currentPage, limit]);
+
+  useEffect(() => {
+    setSearchParams({ page: currentPage, limit: limit });
+  }, [currentPage, limit]);
 
   const handleLogout = () => {
     localStorage.clear();
     toast.success("User Logged out!");
     navigate("/users/login");
+  };
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
   };
 
   return (
@@ -58,30 +103,60 @@ function TodoHome() {
             Change Password
           </div>
         </div>
+        <div className="search-box">
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="Search Todo text here"
+          />
+        </div>
       </div>
       <CreateTodo updateRefresh={updateRefresh} />
       {todos.length === 0 ? (
-        <h2>Your todo list is empty.</h2>
+        <h2>No todos to show.</h2>
       ) : (
-        <div className="todos-card">
-          {spinner && (
-            <div className="spinner">
-              <InfinitySpin />
+        <>
+          <div className="todos-card">
+            {spinner && (
+              <div className="spinner">
+                <InfinitySpin />
+              </div>
+            )}
+            <ul>
+              {todos.map((todo, index) => (
+                <li key={todo._id}>
+                  <TodoItem
+                    id={todo._id}
+                    text={todo.text}
+                    status={todo.status}
+                    updateRefresh={updateRefresh}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="pages">
+            <Pagination
+              color="primary"
+              defaultPage={1}
+              count={Math.ceil(totalPagesCount / limit)}
+              page={currentPage}
+              onChange={handlePageChange}
+            />
+            <div className="limit-selection">
+              Showing {limit} items per Page
+              <select
+                name="limit"
+                value={limit}
+                onChange={(e) => setLimit(parseInt(e.target.value))}
+              >
+                <option value="3">3</option>
+                <option value="5">5</option>
+                {/* <option value="10">10</option> */}
+              </select>
             </div>
-          )}
-          <ul>
-            {todos.map((todo, index) => (
-              <li key={todo._id}>
-                <TodoItem
-                  id={todo._id}
-                  text={todo.text}
-                  status={todo.status}
-                  updateRefresh={updateRefresh}
-                />
-              </li>
-            ))}
-          </ul>
-        </div>
+          </div>
+        </>
       )}
     </>
   );
